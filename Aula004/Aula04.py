122b929c363d44a62256e1c622e64f5385523f71
João Inácio <joaoinacio206@protonmail.com>
2021-05-27 15:35:03 -0300

    Create Aula04.py

---
+++ Aula004/Aula04.py
@@ -0,0 +1,21 @@
+"""
+Tipos de Dados
+str - string
+ int - inteiro
+ float - real
+ bool - lógico
+"""
+'''print(type('Luiz'))
+print(type(1))
+print(type(1.5))
+print(type(True))
+print(type('Oi'), type(10), type(1.5), type(True))'''
+
+# Str
+print('João', type('João'))
+# Int
+print(22, type(22))
+# float
+print(1.50, type(1.50))
+# Bool
+print(22 > 18, type(22 > 18))
