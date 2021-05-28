e7621205d917ef157fee9c359826effddc2ef3a1
João Inácio <joaoinacio206@protonmail.com>
2021-05-28 09:54:04 -0300

    UP

---
+++ Aula005/aula05.py
@@ -0,0 +1,21 @@
+"""
+Operadores Aritméticos
+
++ = Soma
+- = Subtração
+* = Multiplicação
+/ = Divisão
+// = Divisão inteira
+** = Potenciação
+% = Modulo (Resto da divisãa)
+"""
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+print(10 // 10)
+print(10 ** 10)
+print(10 % 15)
+
+
+
---
+++ Pratica/aula.py
@@ -0,0 +1,27 @@
+"""print("Hello word!")
+print('Olá Mundo!')"""
+n1 = int(input("Primeiro Número: "))
+n2 = int(input("Segundo Número: "))
+op = input("Qual operação você quer fazer:\n"
+           "[+] = Soma\n"
+           "[-] = Subtração\n"
+           "[*] = Multiplicação\n"
+           "[/] = Divisão\n"
+           ": ")
+if op == "+":
+    r1 = n1 + n2
+    print("O resultado é: ", r1)
+elif op == "-":
+    r2 = n1 - n2
+    print("O resultado é: ", r2)
+elif op == "*":
+    r3 = n1 * n2
+    print("O resultado é: ", r3)
+elif op == "/":
+    r4 = n1 // n2
+    print("O resultado é: ", r4)
+else:
+    print("Verifique o tipo de operação")
+
+
+
