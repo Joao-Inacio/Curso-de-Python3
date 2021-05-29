5c9fddd04ffaa9def0ac217ee38cdd57127231d0
João Inácio <joaoinacio206@protonmail.com>
2021-05-29 11:24:36 -0300

    Create aula07.py

---
+++ Aula007/aula07.py
@@ -0,0 +1,11 @@
+"""
+Exercício da Aula 06
+"""
+nome = "Ana"
+idade = 25
+altura = 1.69
+e_maior = idade > 18
+peso = 65
+imc = peso / (altura * altura)
+# print(nome, "Tem", idade, "Anos e seu IMC é", imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
