b03c4415ed574ee5d06403be5b78bdf85f0df9ce
João Inácio <joaoinacio206@protonmail.com>
2021-05-29 13:27:48 -0300

    Create exercicio.py

---
+++ Aula007/exercicio.py
@@ -0,0 +1,12 @@
+
+nome = 'Paulo Oliveira'
+idade = 30
+altura = 1.79
+peso = 85
+ano_atual = 2021
+imc = peso / (altura * altura)
+ano_nascimento = ano_atual - idade
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}.\n'
+      f'O IMC de {nome} é {imc:.2f}.\n'
+      f'{nome} nasceu em {ano_nascimento}')
+
