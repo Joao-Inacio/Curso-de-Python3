d926eeb4bbee04ef4507e534ee59ba9fea438949
João Inácio <joaoinacio206@protonmail.com>
2021-06-01 08:29:31 -0300

    Up

---
+++ Aula010/aula10.py
@@ -0,0 +1,8 @@
+"""
+    Operadores Relacionais
+    == > >= < <= !=
+"""
+n1 = 2
+n2 = 2
+exp = (n1 != n2)
+print(exp)
---
+++ Aula011/aula11.py
@@ -0,0 +1,9 @@
+"""
+Operadores Lógicos
+and, or, not
+in, e not in
+"""
+n1 = 15
+n2 = 15
+if n1 == n2 and 'a' == 'a':
+    print('Verdadeiro com AND')
