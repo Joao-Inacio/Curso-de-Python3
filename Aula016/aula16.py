7097e602342c2df5fc7d31eb2a4fa827eb98d54c
João Inácio <joaoinacio206@protonmail.com>
2021-06-05 15:20:04 -0300

    Create aula16.py

---
+++ Aula016/aula16.py
@@ -0,0 +1,12 @@
+"""
+Manipulando strings
+* String indices
+*Fatiamento de str [inc:fim:passo]
+*Função built-in len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo.
+"""
+# positivos
+# negativos
+texto = 'Python s2'
+nova_str = texto[0::2]
+print(nova_str)
