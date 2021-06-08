6e6819eed4798bfbcb4d97e09f263bf4e29d1928
João Inácio <joaoinacio206@protonmail.com>
2021-06-08 09:54:25 -0300

    Create aula18.py

---
+++ Aula018/aula18.py
@@ -0,0 +1,15 @@
+"""
+While / Else
+Contadores
+Acumuladores
+"""
+contador = 1
+acumulador = 1
+while contador <= 10:
+    print(contador, acumulador)
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('Cheguei no else.')
+
+
