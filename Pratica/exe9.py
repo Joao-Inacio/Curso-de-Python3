d3391c964968b3c00db9a1433db3a2b97f5b008a
João Inácio <joaoinacio206@protonmail.com>
2021-06-22 13:31:51 -0300

    Create exe9.py

---
+++ Pratica/exe9.py
@@ -0,0 +1,13 @@
+'''n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+s = (n1 * n2) / 100
+print(s)'''
+
+
+def funcao(n1, n2):
+    soma = (n1 * n2) / 100
+    total = n1 + soma
+    return total
+
+
+print(funcao(500, 35))
