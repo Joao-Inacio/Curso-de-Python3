1d6ffa5d25ab7383e95322d6fccdaff10ebc0186
João Inácio <joaoinacio206@protonmail.com>
2021-06-27 09:46:02 -0300

    Create exe11.py

---
+++ Pratica/exe11.py
@@ -0,0 +1,14 @@
+"""
+crie uma função1 que receba uma função 2 como parâmetro e retorne o valor da função 2 executada
+"""
+
+
+def func1():
+    print(func2())
+
+
+def func2():
+    return 'eai'
+
+
+func1()
