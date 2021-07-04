23376319f372119cbef970e7825b3ec8ae32f4df
João Inácio <joaoinacio206@protonmail.com>
2021-07-04 10:38:28 -0300

    Create aula37.py

---
+++ Aula037/aula37.py
@@ -0,0 +1,35 @@
+"""
+Exercícios
+"""
+lista_de_li_ints = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+
+# não consigui só
+
+def encontra_first_dupli(param_lis_de_ints):
+    numeros_check = set()
+    first_dupli = -1
+
+    for numero in param_lis_de_ints:
+        if numero in numeros_check:
+            first_dupli = numero
+            break
+        numeros_check.add(numero)
+    return first_dupli
+
+
+for lis_de_ints in lista_de_li_ints:
+    print(lis_de_ints, encontra_first_dupli(lis_de_ints))
