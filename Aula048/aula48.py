280b4a803695cbfa701fb7b1c4c41cae80323b79
João Inácio <joaoinacio206@protonmail.com>
2021-07-15 11:25:43 -0300

    Create aula48.py

---
+++ Aula048/aula48.py
@@ -0,0 +1,28 @@
+"""
+Groupby - Agrupando valores
+"""
+from itertools import groupby, tee
+
+alunos = [
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'C'},
+    {'nome': 'Anderson', 'nota': 'B'},
+]
+ordena = lambda item: item['nota']
+alunos.sort(key=ordena)
+alunos_agrupados = groupby(alunos, ordena)
+
+for agrupamento, valores_agrupados in alunos_agrupados:
+    va1, va2 = tee(valores_agrupados)
+    print(f'Agrupamento: {agrupamento}')
+    for alunos in va1:
+        print(f'\t{alunos}')
+    quantidade = len(list(va2))
+    print(f'\t{quantidade} alunos tiraram a nota {agrupamento}')
+    print()
