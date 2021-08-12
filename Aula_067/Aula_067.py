e90a91a3eb711eb56f0a55d21ac3337155d1ae97
João Inácio <joaoinacio206@protonmail.com>
2021-08-12 19:09:02 -0300

    SETTER e GETTER Pt2

--- Aula_067/Aula_067.py
+++ Aula_067/Aula_067.py
@@ -1,18 +1,18 @@
 # SETTER = CONFIGURANDO UM VALOR (=)
 # GETTER = OBTER UM VALOR (.)
 class Pessoa:
-    def __init__(self):
-        self.atributo = 'Inicial'
+    def __init__(self, nome):
+        self._nome = nome
 
-    @property
+    @property  # GETTER
     def nome(self):
-        return 'João Inácio'
+        return self._nome  # Não se use assim self.nome no GETTER e no SETTER Loop
 
-    @nome.setter
+    @nome.setter  # SETTER
     def nome(self, nome):
-        self.atributo = nome
+        self._nome = nome
 
 
-p1 = Pessoa()
-print(p1.atributo)
+p1 = Pessoa('João')
+p1.nome = 'Otávio'
 print(p1.nome)  # Não se colocar parenteses
