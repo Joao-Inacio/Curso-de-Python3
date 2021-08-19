ddd1ab8d353c6651f1384c50398e90b142fe42f6
João Inácio <joaoinacio206@protonmail.com>
2021-08-19 18:54:22 -0300

    Herança múltipla - Python Orientado a Objetos
    Parte 2

--- Aula_073/Aula73.py
+++ Aula_073/Aula73.py
@@ -1,4 +1,15 @@
 """
     Herança múltipla - Python Orientado a Objetos
 """
+from smartphone import Smartphone
 
+smartphone = Smartphone('Pocophone F1 ')
+smartphone.conectar()
+smartphone.desligar()
+smartphone.ligar()
+smartphone.conectar()
+smartphone.conectar()
+smartphone.conectar()
+smartphone.desligar()
+smartphone.conectar()
+smartphone.desconectar()
--- Aula_073/smartphone.py
+++ Aula_073/smartphone.py
@@ -1,26 +1,39 @@
 from Aula_073.eletronico import Eletronico
+from log import LogMixin
 
 
-class Smartphone(Eletronico):
+class Smartphone(Eletronico, LogMixin):
     def __init__(self, nome):
         super().__init__(nome)
         self._conectado = False
 
     def conectar(self):
         if not self._ligado:
-            print(f'{self._nome} não está ligado')
+            info = f'{self._nome} não está ligado'
+            print(info)
+            self.log_error(info)
             return
 
         if self._conectado:
-            print(f'{self._nome} Já está conectado.')
+            error = f'{self._nome} Já está conectado.'
+            print(error)
+            self.log_error(error)
             return
 
+        info = f'{self._nome}está conectado.'
+        print(info)
+        self.log_info(info)
         self._conectado = True
 
     def desconectar(self):
         if not self._conectado:
-            print(f'{self._nome} não está conectado.')
+            error = f'{self._nome} não está conectado.'
+            print(error)
+            self.log_error(error)
             return
+        info = f'{self._nome} foi desligado'
+        print(info)
+        self.log_info(info)
         self._ligado = False
 
 
