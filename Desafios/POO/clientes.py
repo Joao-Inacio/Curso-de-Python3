a41902496c68a10899b62d3186a0812c6fea6c07
João Inácio <joaoinacio206@protonmail.com>
2021-09-06 18:32:07 -0300

    Exercício com Abstração, Herança, Encapsulamento e Polimorfismo

---
+++ Desafios/POO/clientes.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self._nome = nome
+        self._idade = idade
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def idade(self):
+        return self._idade
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.conta = None
+
+    def inserir_conta(self, conta):
+        self.conta = conta
+
+
+class Conta(ABC):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        self.saldo += valor
+        self.detalhes()
+
+    def detalhes(self):
+        print(f'Agência: {self.agencia} '
+              f'Conta: {self.conta} '
+              f'Saldo: {self.saldo} ')
+
+    @abstractmethod
+    def sacar(self, valor):
+        pass
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if self.saldo < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite=100):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        if (self.saldo + self.limite) < valor:
+            print('Saldo insuficiente')
+            return
+
+        self.saldo -= valor
+        self.detalhes()
