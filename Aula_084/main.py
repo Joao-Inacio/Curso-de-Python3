b8ddaf4cac788f18fe9498fd93795c363bc49d66
João Inácio <joaoinacio206@protonmail.com>
2021-09-10 14:56:48 -0300

    Implementando um iterator

---
+++ Aula_084/main.py
@@ -0,0 +1,35 @@
+"""
+    Implementando um iterator
+"""
+
+
+class MinhaLista:
+    def __init__(self):
+        self.__items = []
+        self.__index = 0
+
+    def add(self, valor):
+        self.__items.append(valor)
+
+    def __getitem__(self, index):
+        return self.__items[index]
+
+    def __enter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            item = self.__items[self.__index]
+            self.__index += 1
+            return item
+        except IndexError:
+            raise StopIteration
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.__items})'
+
+
+if __name__ == '__main__':
+    lista = MinhaLista()
+    lista.add('João')
+    lista.add('Maria')
