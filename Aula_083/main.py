9c84169bef6970fbf323d85887747697c88b20f8
João Inácio <joaoinacio206@protonmail.com>
2021-09-10 11:14:09 -0300

    Enum

---
+++ Aula_083/main.py
@@ -0,0 +1,25 @@
+"""
+    Enum
+"""
+from enum import Enum, auto
+
+
+class Directions(Enum):
+    right = auto()
+    left = auto()
+    up = auto()
+    down = auto()
+
+
+def move(direction):
+    if not isinstance(direction, Directions):
+        raise ValueError("Cannot move in this direction")
+
+    return f'Moving {direction.name}'
+
+
+if __name__ == '__main__':
+    print(move(Directions.right))
+    print(move(Directions.left))
+    print(move(Directions.up))
+    print(move(Directions.down))
