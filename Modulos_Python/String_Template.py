f71cf3260cba7afc436c0ba34b59fa76fce46ff5
João Inácio <joaoinacio206@protonmail.com>
2021-09-21 10:59:47 -0300

    Enviando e-mails com Python

--- Modulos_Python/String_Template.py
+++ Modulos_Python/String_Template.py
@@ -3,10 +3,37 @@
 """
 from string import Template
 from datetime import datetime
+from dados_email import *
 
-with open('template.html', 'r') as html:
+from email import policy
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
+
+with open('template.html', 'r', encoding='utf-8') as html:
     template = Template(html.read())
     hoje = datetime.now().strftime('%d/%m/%Y')
     corpo_msg = template.substitute(nome='João', data=hoje)
 
-print(corpo_msg)
+msg = MIMEMultipart(policy=policy.default)
+
+msg['from'] = 'João Inácio'
+msg['to'] = 'joaoinacio206@gmail.com'
+msg['subject'] = 'Deu certo!!!'
+
+corpo = MIMEText(corpo_msg, 'html')
+msg.attach(corpo)
+
+with open('FeG.jpg', 'rb') as img:
+    img = MIMEImage(img.read())
+    msg.attach(img)
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(my_email, my_semha)
+    smtp.send_message(msg)
+    print('E-mail enviado com sucesso')
+
+
---
+++ Modulos_Python/dados_email.py
@@ -0,0 +1,7 @@
+"""
+    Enviando e-mails com Python
+    dados_email.py
+"""
+
+my_email = 'joaonetoplus@gmail.com'
+my_semha = 'Senha'
