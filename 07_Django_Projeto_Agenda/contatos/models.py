901b6bbec44ed4d1c9286adda6560e1d61f6450a
João Inácio <joaoinacio206@protonmail.com>
2021-10-28 09:31:52 -0300

    configurando  a pagina Admin

--- 07_Django_Projeto_Agenda/contatos/models.py
+++ 07_Django_Projeto_Agenda/contatos/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -15,5 +18,6 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
-
+    def __str__(self):
+        return self.nome
 
