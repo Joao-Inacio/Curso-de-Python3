8d4ff8ee9233abf4f3a75ae1e0905c1dab896a9b
João Inácio <joaoinacio206@protonmail.com>
2021-11-02 18:55:23 -0300

    Levantando o erro 404

--- 07_Django_Projeto_Agenda/contatos/views.py
+++ 07_Django_Projeto_Agenda/contatos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 
 
@@ -10,7 +11,15 @@ def index(request):
 
 
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    """try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404()"""
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
