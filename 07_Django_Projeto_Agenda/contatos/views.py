c49df4c0b2f5be47c5bcffb1a247dee909a71434
João Inácio <joaoinacio206@protonmail.com>
2021-11-04 19:04:40 -0300

    Ordenando e filtrando valores
    E corrigindo o bug de acesso do contato através do id pela a url

--- 07_Django_Projeto_Agenda/contatos/views.py
+++ 07_Django_Projeto_Agenda/contatos/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-# from django.http import Http404
+from django.http import Http404
 from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 10)
 
     page = request.GET.get('p')
@@ -26,6 +28,10 @@ def ver_contato(request, contato_id):
         raise Http404()"""
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
