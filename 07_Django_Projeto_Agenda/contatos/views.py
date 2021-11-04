56dd8b55f66e636e9f71a6248599c56066698d55
João Inácio <joaoinacio206@protonmail.com>
2021-11-04 10:18:07 -0300

    Paginação

--- 07_Django_Projeto_Agenda/contatos/views.py
+++ 07_Django_Projeto_Agenda/contatos/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.core.paginator import Paginator
+# from django.http import Http404
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
