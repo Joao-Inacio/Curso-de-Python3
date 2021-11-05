39e23ee440dd874e4447cf4ca2b8f369b5990178
João Inácio <joaoinacio206@protonmail.com>
2021-11-05 19:03:25 -0300

    Campo de pesquisa
    Campo de pesquisa mais a paginação dos resultados

--- 07_Django_Projeto_Agenda/contatos/views.py
+++ 07_Django_Projeto_Agenda/contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -34,4 +36,30 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404()
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    """contatos = Contato.objects.order_by('id').filter(
+        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+        mostrar=True
+    )"""
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
