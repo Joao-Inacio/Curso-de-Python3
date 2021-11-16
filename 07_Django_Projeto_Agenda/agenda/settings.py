a37208a7d7a7f39c0a09b5b14deed91f1cf31f19
João Inácio <joaoinacio206@protonmail.com>
2021-11-16 09:03:39 -0300

    Verificando usuários logados
    Correção dos títulos das pagina e verificação se o usuário já está logado

--- 07_Django_Projeto_Agenda/agenda/settings.py
+++ 07_Django_Projeto_Agenda/agenda/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-xdj*ig@7!!96f_vsbh$srgi@tb9%4u%#vb03#_d=qnp^gm*(de'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
