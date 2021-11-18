659af3572c08360ac8f8bc38279f2141e6e19c5c
João Inácio <joaoinacio206@protonmail.com>
2021-11-18 13:57:14 -0300

    Preparando o projeto para o deploy

--- 07_Django_Projeto_Agenda/agenda/settings.py
+++ 07_Django_Projeto_Agenda/agenda/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xdj*ig@7!!96f_vsbh$srgi@tb9%4u%#vb03#_d=qnp^gm*(de'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR.joinpath(BASE_DIR, 'templates/static')
 ]
+STATIC_ROOT = BASE_DIR.joinpath('static')
+
 # Mensagens
 MESSAGE_TAGS = {
     constants.ERROR: 'alert-danger',
