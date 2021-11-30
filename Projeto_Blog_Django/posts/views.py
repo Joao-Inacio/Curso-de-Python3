a690bc4d61bdba8b334531eaa8a35fda46c69b37
João Inácio <joaoinacio206@protonmail.com>
2021-11-30 11:09:32 -0300

    Implementação da visualização dos comentários

--- Projeto_Blog_Django/posts/views.py
+++ Projeto_Blog_Django/posts/views.py
@@ -68,6 +68,16 @@ class PostDetalhes(UpdateView):
     form_class = FormComentario
     context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comentario = Comentario.objects.filter(publicado_comentario=True,
+                                               post_comentario=post.id)
+        contexto['comentarios'] = comentario
+
+
+        return contexto
+
     def form_valid(self, form):
         post = self.get_object()
         comentario = Comentario(**form.cleaned_data)
