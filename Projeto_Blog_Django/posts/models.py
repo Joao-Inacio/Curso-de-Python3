8353b1329595104b42894a4f4116b567fc356084
João Inácio <joaoinacio206@protonmail.com>
2021-12-06 18:37:04 -0300

    Redimensionado imagens no model do post

--- Projeto_Blog_Django/posts/models.py
+++ Projeto_Blog_Django/posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -17,3 +20,27 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo_post
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.resizer_image(self.imagem_post.name, 800)
+
+    @staticmethod
+    def resizer_image(img_name, new_width):
+        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = round((new_width * height) / width)
+
+        if width <= new_width:
+            img.close()
+            return
+
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            img_path,
+            optimize=True,
+            quality=60
+        )
+        new_img.close()
