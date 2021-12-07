33d8c63aa7f4e5d301c3e3c1f31c2a5b6810cc03
João Inácio <joaoinacio206@protonmail.com>
2021-12-07 18:33:18 -0300

    implementando o reCAPTCHA e finalizando o projeto do blog

--- Projeto_Blog_Django/comentarios/forms.py
+++ Projeto_Blog_Django/comentarios/forms.py
@@ -1,19 +1,36 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
-
-        if len(comentario) < 1:
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+        #
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6Le-2YYdAAAAAA24uF47Z8LzzVUZFg1XMJf5SUqR',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+        if not recaptcha_result.get('success'):
             self.add_error(
                 'comentario',
-                'O campo comentario não pode está vazio'
+                'falha na validação'
             )
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
+
+        # if len(comentario) < 1:
+        #     self.add_error(
+        #         'comentario',
+        #         'O campo comentario não pode está vazio'
+        #     )
 
     class Meta:
         model = Comentario
