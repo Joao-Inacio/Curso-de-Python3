3fbd705f2e541576a745909d39b0d96131218d5e
João Inácio <joaoinacio206@protonmail.com>
2021-12-09 19:07:46 -0300

    Criando os models Produto e Variação
    Também implementando o redimensionamento  de imagens

--- Progeto_Ecommerce/produto/admin.py
+++ Progeto_Ecommerce/produto/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
+from .models import Produto, Variacao
 
-# Register your models here.
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Variacao)
