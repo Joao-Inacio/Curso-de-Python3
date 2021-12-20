ecebf6cce45ad40224cae04c0457052b973b927c
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2021-12-20 12:10:37 -0300

    Detalhes do produto

--- Projeto_Ecommerce/produto/views.py
+++ Projeto_Ecommerce/produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -8,11 +9,14 @@ class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
-    paginate_by = 3
+    paginate_by = 4
 
 
-class DetalheProdutos(View):
-    pass
+class DetalheProdutos(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
