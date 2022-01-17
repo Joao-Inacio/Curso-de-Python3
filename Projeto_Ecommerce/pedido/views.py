6ded9715160cf7ea5c2bacbb741bb2c9c2b1593e
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-01-17 10:43:51 -0300

    Salvando o pedido (Parte 2)

--- Projeto_Ecommerce/pedido/views.py
+++ Projeto_Ecommerce/pedido/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
@@ -8,7 +9,27 @@ from utils import utils
 from .models import Pedido, ItemPedido
 
 
-class Pagar(View):
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+        return super().dispatch(*args, **kwargs)
+        
+
+
+class Pagar(DispatchLoginRequired, DetailView):
+    template_name = 'pedido/pagar.html'
+    model = Pedido
+    pk_url_kwargs = 'pk'
+    context_object_name = 'pedido'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(usuario=self.request.user)
+        return qs
+
+
+class SalvaPedido(View):
     template_name = 'pedido/pagar.html'
 
     def get(self, *args, **kwargs):
@@ -82,11 +103,14 @@ class Pagar(View):
         )
 
         del self.request.session['carrinho']
-        return redirect('pedido:lista')
-
-
-class SalvaPedido(View):
-    pass
+        return redirect(
+            reverse(
+                'pedido:pagar',
+                kwargs={
+                    'pk': pedido.pk
+                }
+            )
+        )
 
 
 class Detalhe(View):
