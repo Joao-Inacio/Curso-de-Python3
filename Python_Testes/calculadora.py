174fb15e13654e28713315bb39e6db54a60b81d2
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-01-26 10:44:41 -0300

    Asserções (Assertions)

--- Python_Testes/calculadora.py
+++ Python_Testes/calculadora.py
@@ -1,2 +1,4 @@
 def soma(x, y):
+    assert isinstance(x, (int, float)), 'X precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'Y precisa ser int ou float'
     return x + y
--- Python_Testes/main.py
+++ Python_Testes/main.py
@@ -1,5 +1,6 @@
 from calculadora import soma    
 
-print(soma(10, 20))
-print(soma(-10, 20))
-print(soma(1.5, 2.5))
+# print(soma(10, 20))
+# print(soma(-10, 20))
+# print(soma(1.5, 2.5))
+print(soma('15', '15'))
