073ee62ee23e0540981e6e0e4b3e95bf46303b48
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-02-07 10:50:35 -0300

    Unittest #3 - Com TDD

---
+++ Python_Testes/test_pessoa.py
@@ -0,0 +1,57 @@
+import unittest
+from unittest.mock import patch
+from Pessoa import Pessoa
+
+
+class TestPessoa(unittest.TestCase):
+    def setUp(self):
+        self.p1 = Pessoa('João', 'Inácio')
+
+    def test_pessoa_attr_nome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.nome, 'João')
+
+    def test_pessoa_attr_nome_e_str(self):
+        self.assertIsInstance(self.p1.nome, str)
+
+    def test_pessoa_attr_sobrenome_tem_o_valor_correto(self):
+        self.assertEqual(self.p1.sobrenome, 'Inácio')
+
+    def test_pessoa_attr_sobrenome_e_str(self):
+        self.assertIsInstance(self.p1.sobrenome, str)
+
+    def test_pessoa_attr_dados_obtidos_inicia_false(self):
+        self.assertFalse(self.p1.dados_obtidos)
+
+    def test_pessoa_attr_dados_obtidos_e_bool(self):
+        self.assertIsInstance(self.p1.dados_obtidos, bool)
+
+    def test_obter_todos_os_dados_sucesso_OK(self):
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p1.dados_obtidos)
+
+    def test_obter_todos_os_dados_falha_404(self):
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p1.dados_obtidos)
+    
+    def test_obter_todos_os_dados_sucesso_e_falha_sequencial(self):
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'CONECTADO')
+            self.assertTrue(self.p1.dados_obtidos)
+            
+            fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.obter_todos_os_dados(), 'ERRO 404')
+            self.assertFalse(self.p1.dados_obtidos)
+            
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
