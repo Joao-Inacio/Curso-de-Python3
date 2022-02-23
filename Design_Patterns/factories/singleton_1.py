8169be1781d66bc84df22786b3836efa6b220ca9
João Inácio <73400085+Joao-Inacio@users.noreply.github.com>
2022-02-23 09:17:08 -0300

    Singleton #1 (Criação)

---
+++ Design_Patterns/factories/singleton_1.py
@@ -0,0 +1,18 @@
+from pip import main
+
+
+class AppSettings:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+
+if __name__ == '__main__':
+    as1 = AppSettings()
+    as2 = AppSettings()
+
+    print(as1)
+    print(as2)
